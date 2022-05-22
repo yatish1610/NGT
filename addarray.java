a8d48c5968c199e009fd333410d13481f6bf3b1c
yatish1610 <93828505+yatish1610@users.noreply.github.com>
2022-05-23 02:34:27 +0530

    Add files via upload

---
+++ addarray.java
@@ -0,0 +1,21 @@
+package test;
+
+public class addmatrix {
+	public static void main(String args[]){  
+		 
+		int x[][]={{4,1,5},{2,0,3},{2,4,5}};    
+		int y[][]={{2,8,3},{1,4,3},{1,2,1}};    
+		    
+		   
+		int z[][]=new int[3][3]; 
+		    
+	   
+		for(int i=0;i<3;i++){    
+		for(int j=0;j<3;j++){    
+		z[i][j]=a[i][j]+b[i][j];     
+		System.out.print(z[i][j]+" ");    
+		}    
+		System.out.println();   
+		}    
+		}
+            }  
